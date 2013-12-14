4e5adb8df89b2e888b53339cd4631fb2bc3e2df7
Stas Frid <StasAleinikov@gmail.com>
2013-12-14 20:08:26 +0200

    discoAPI search added

---
+++ discotech/discoAPI/discoAPISearcher.py
@@ -0,0 +1,46 @@
+import json
+import urllib
+import requests
+import discotech
+
+__package__ = 'discotech.discoAPI'
+
+class DiscoAPISearcher(object):
+    """
+    a class to search results from cached data in discoAPI
+    """
+
+    def __init__(self,discoAPIKey = ""):
+        self.__discoAPIKey = discoAPIKey
+
+
+    def setDiscoAPIKey(self,api_key):
+        """
+        set you discoAPIKey
+
+        @param api_key: your discoAPI key
+        @type  api_key: str
+        """
+        self._discoAPIKey = api_key
+
+        
+    def search(self,**kwargs):
+        """
+        search discoAPI using API parametars
+        """
+
+        if self.__discoAPIKey == "":
+            raise discotech.discotechError('you must supply an api key')
+        
+        # prepare your parametars
+        data = {'api_key' : self.__discoAPIKey}
+        # add arguments
+        data.update(**kwargs)
+        # prepare url
+        url = 'https://discoapi.com/api/query/'+urllib.quote(json.dumps(data))
+        # get response
+        jsonResponse = requests.get(url)
+        # parse response
+        response = json.loads(jsonResponse.text)
+
+        return response
